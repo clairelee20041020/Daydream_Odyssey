8749dc8533966c00cde5c3e07f8c96b5ead3bc39
smd3010 <yujunfang1013@gmail.com>
2025-05-01 11:03:11 +0800

    main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+﻿#include "config.h"
+#include "game.h"
+#include "stateController.h"
+
+int main()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib:: Minesweeper");
+    SetTargetFPS(60);//把while(!WindowShouldClose)控制在每1/60秒跑一次
+    GameStartup();
+
+    while (!WindowShouldClose()) {
+        if (currentState && currentState->update) currentState->update();
+
+        BeginDrawing();
+        ClearBackground(DARKGREEN);
+        if (currentState && currentState->render) currentState->render();
+        EndDrawing();
+    }
+
+    GameShutdown();
+    CloseWindow();
+
+    return 0;
+}
